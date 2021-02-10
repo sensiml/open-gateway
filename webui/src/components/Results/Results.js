b5412c61e8c14aa88f444528f853ab1aa6c2a3dc
Chris Knorowski <cknorow@gmail.com>
2021-02-10 10:08:15 -0800

    UI/UX enhancements to Streaming Page (#7)
    * adding filter results and class map options

--- webui/src/components/Results/Results.js
+++ webui/src/components/Results/Results.js
@@ -3,29 +3,33 @@ import axios from "axios";
 import { Typography } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { DataGrid } from "@material-ui/data-grid";
+import ResultsFilter from "./ResultsFilter";
+import Slider from "@material-ui/core/Slider";
+import Divider from "@material-ui/core/Divider";
 
 var id_counter = 0;
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
   controls: {
-    display: "flex",
+    display: "flex-right",
     alignItems: "center",
     paddingLeft: theme.spacing(1),
     paddingBottom: theme.spacing(1),
+  },
+  sliderroot: {
+    width: 250,
+  },
+  sliderinput: {
+    width: 42,
+  },
+  section1: {
+    margin: theme.spacing(3, 2),
+  },
+  section2: {
+    margin: theme.spacing(2),
+    textAlign: "center",
   },
 }));
 
@@ -89,47 +93,98 @@ const Results = (props) => {
     { field: "ModelNumber", headerName: "Model ID", width: 240 },
     { field: "Classification", headerName: "Classification", width: 240 },
   ]);
+  const [filterLength, setfilterLength] = React.useState(1);
 
   const classes = useStyles();
   const theme = useTheme();
 
+  const handleFilterLengthSliderChange = (event, newValue) => {
+    console.log(newValue);
+    setfilterLength(newValue);
+  };
+
   return (
     <Grid>
-      <div className={classes.details}>
-        <Typography component="h5" variant="h5">
-          Model Result
-        </Typography>
-        <Typography variant="subtitle1" color="textSecondary"></Typography>
-        <div className={classes.controls}>
-          <Button
-            aria-label="disconnect"
-            onClick={() => {
-              handleStreamRequest(
-                "clicked",
-                `${process.env.REACT_APP_API_URL}results`,
-                props.setDeviceRows
-              );
-            }}
-          >
-            Connect
-          </Button>
-          <Button
-            aria-label="disconnect"
-            onClick={() => {
-              handleDisconnectRequest("clicked");
-            }}
-          >
-            Disconnect
-          </Button>
-        </div>
+      <div className={classes.section1}>
+        <Grid container spacing={2} rows>
+          <Grid item xs={8}>
+            <Typography component="h3" variant="h3" color="secondary">
+              Mode: Recogntion
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <div className={classes.controls}>
+              <Button
+                aria-label="disconnect"
+                color="primary"
+                variant="contained"
+                onClick={() => {
+                  handleStreamRequest(
+                    "clicked",
+                    `${process.env.REACT_APP_API_URL}results`,
+                    props.setDeviceRows
+                  );
+                }}
+              >
+                Connect
+              </Button>
+            </div>
+          </Grid>
+          <Grid item>
+            <div className={classes.controls}>
+              <Button
+                aria-label="disconnect"
+                color="secondary"
+                variant="contained"
+                onClick={() => {
+                  handleDisconnectRequest("clicked");
+                }}
+              >
+                Disconnect
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
       </div>
-      <div style={{ height: 600, width: "100%" }}>
-        <DataGrid
-          rows={props.deviceRows}
-          columns={deviceColumns}
-          pageSize={15}
-          sortModel={[{ field: "id", sort: "desc" }]}
-        />
+      <Divider variant="middle" />
+      <div className={classes.section2}>
+        <Grid alignContent="center">
+          <ResultsFilter
+            data={props.deviceRows}
+            filter_length={filterLength}
+          ></ResultsFilter>
+        </Grid>
+      </div>
+      <Divider variant="middle" />
+      <div className={classes.section1}>
+        <Grid container spacing={3} alignItems="center">
+          <Grid item> Filter Length </Grid>
+          <div className={classes.sliderroot}>
+            <Grid item xs>
+              <Slider
+                value={typeof filterLength === "number" ? filterLength : 1}
+                onChange={handleFilterLengthSliderChange}
+                aria-labelledby="input-slider"
+                min={1}
+                max={10}
+              />
+            </Grid>
+          </div>
+          <Grid item> {filterLength} </Grid>
+        </Grid>
+      </div>
+      <Divider variant="middle" />
+
+      <div className={classes.section1}>
+        <div style={{ height: 600, width: "100%" }}>
+          <DataGrid
+            rows={props.deviceRows}
+            columns={deviceColumns}
+            pageSize={15}
+            sortModel={[{ field: "id", sort: "desc" }]}
+          />
+        </div>
       </div>
     </Grid>
   );
---
+++ webui/src/components/Results/ResultsFilter.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const ResultsFilter = (props) => {
+  function filterData(data, filter_length) {
+    if (data.length === 0) {
+      return "No Results";
+    }
+
+    if (filter_length > data.length) {
+      filter_length = data.length - 1;
+    }
+
+    let m = {};
+    for (var i = data.length - filter_length; i < data.length; i++) {
+      if (data[i].Classification in m) {
+        m[data[i].Classification] += 1;
+      } else {
+        m[data[i].Classification] = 1;
+      }
+    }
+
+    let index = 0;
+    let max = 0;
+    for (const [key, value] of Object.entries(m)) {
+      if (value > max) {
+        max = value;
+        index = key;
+      }
+    }
+
+    if (max < filter_length / 2) {
+      return "UNC";
+    }
+    return index;
+  }
+
+  return (
+    <Typography variant="h1" component="h2">
+      {filterData(props.data, props.filter_length)}
+    </Typography>
+  );
+};
+
+export default ResultsFilter;
